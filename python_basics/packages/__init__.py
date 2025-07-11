1ac6bcff0bf29370c247f7267065a5ee3bac8967
kalyan199999 <gottapupavankalyan@gmail.com>
2025-07-11 19:29:55 +0530

    files updated!

---
+++ python_basics/packages/__init__.py
@@ -0,0 +1,56 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "eb16db2b-ff24-4e76-9f9a-5566a3d672b9",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'\\n__init__.py is a special type of file used in python to define the packages and initialize their namespaces\\n'"
+      ]
+     },
+     "execution_count": 1,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "'''\n",
+    "__init__.py is a special type of file used in python to define the packages and initialize their namespaces\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "898cb023-7624-48bc-9571-5029b7296e51",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ python_basics/packages/maths.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "3423ad65-153b-4ab7-97eb-a4aee15624ff",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def addition(a=0,b=0):\n",
+    "    return a+b"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "d275aa33-35f1-49e7-8e77-9c7d53ad0dda",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "subtraction = lambda a,b=0 : a-b"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "0777ff4a-fd3e-4e4d-bcee-9461c5438806",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "check_even = lambda x : x%2 == 0"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d3745d11-244d-40b3-8e8a-8de68601f690",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
