58fb69aa938530176fe1bf1f65e0a5f99723a2e5
kalyan199999 <gottapupavankalyan@gmail.com>
2025-07-11 19:29:55 +0530

    files updated!

---
+++ python_basics/packages/.ipynb_checkpoints/maths-checkpoint.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3423ad65-153b-4ab7-97eb-a4aee15624ff",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def addition(a=0,b=0):\n",
+    "    return a+b"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
