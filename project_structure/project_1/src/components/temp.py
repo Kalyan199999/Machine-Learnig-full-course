5b9c8f98dc0b90f9a6c5e293fda70ad9cf441b95
kalyan199999 <gottapupavankalyan@gmail.com>
2025-09-08 17:42:09 +0530

    data injection

---
+++ project_structure/project_1/src/components/temp.py
@@ -0,0 +1,7 @@
+import os
+
+# ../../All_data_sets/eda_dataset/StudentsPerformance.csv
+path = '../../notebook/'
+
+print("Current working directory:", os.getcwd())
+print("Looking for file at:", os.path.abspath(path))
