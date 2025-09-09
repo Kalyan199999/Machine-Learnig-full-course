8dc09ffa47d4e571bf14bc35b399b26dcdc3fa08
kalyan199999 <gottapupavankalyan@gmail.com>
2025-09-09 13:16:21 +0530

    Model training is done!

--- project_structure/project_1/src/utils.py
+++ project_structure/project_1/src/utils.py
@@ -8,6 +8,9 @@ from src.logger import logging
 
 import dill
 
+from sklearn.metrics import r2_score
+# mean_squared_error, mean_absolute_error, 
+
 def save_object(file_path, obj):
     
     """
@@ -20,6 +23,29 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for key, model in models.items():
+            logging.info(f"Training {key} model")
+            model.fit(X_train, y_train)
+
+       
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[key] = test_score
+            logging.info(f"Model {key} Training is done!")
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
