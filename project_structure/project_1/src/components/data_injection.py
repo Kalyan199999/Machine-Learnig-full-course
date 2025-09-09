bd5b6caa21f199ccf60a79c228d9dae8b75743f6
kalyan199999 <gottapupavankalyan@gmail.com>
2025-09-09 13:16:21 +0530

    Model training is done!

--- project_structure/project_1/src/components/data_injection.py
+++ project_structure/project_1/src/components/data_injection.py
@@ -9,6 +9,10 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTranformationConfig
 from src.components.data_transformation import DataTransformation 
+from src.components.model_trainer import (
+    ModelTrainerConfig,
+    ModelTrainer
+)
 
 @dataclass
 class DataIngestionConfig:
@@ -55,4 +59,12 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
+
     train_arr , test_arr , preprocessor_obj_file_path = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+
+    print(
+        model_trainer.initiate_model_trainer(train_arr, test_arr, preprocessor_obj_file_path)
+    )
+
