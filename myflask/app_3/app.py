0ed484ca02cbc3b490a34abe607c42490aff49be
kalyan199999 <gottapupavankalyan@gmail.com>
2025-10-26 16:30:02 +0530

    flask files are added!

--- myflask/app_3/app.py
+++ myflask/app_3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/success')
+def success():
+    score = int(request.args.get('score', 0))
+    res = "passed" if score >= 60 else "failed"
+    return render_template('result.html', result=res , marks=score)
+
 if __name__ == "__main__":
-    app.run( debug=True )
+    app.run(debug=True)
